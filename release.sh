e4eae79419b3f9961ba0898d7f4bc4f3737c5393
Lauri Ojansivu <x@xet7.org>
2022-12-25 04:50:40 +0200

    Updated dependencies.

--- release.sh
+++ release.sh
@@ -3,12 +3,19 @@
 if [ $# -ne 1 ]
   then
     echo "Syntax with Wekan version number:"
-    echo "  ./release.sh 1.1.3"
+    echo "  ./release.sh 1.1.4"
     exit 1
 fi
 
+cd wekan
+microk8s.helm dependency update
+cd ..
+git add --all
+git commit -m "Updated dependencies."
+git push
 tar -cvzf wekan-$1.tgz wekan
 mv wekan-$1.tgz ..
 git checkout gh-pages
 mv ../wekan-$1.tgz .
 echo "Update release sha256sum to release list."
+helm dependency build
